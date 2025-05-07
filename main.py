5bfb81915281584c1668240a69a437ef455ab6e9
Gitbottelegramserver <faton1234567891@gmail.com>
2025-05-07 16:02:28 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
 import asyncio
+import threading
+import os
 from aiogram import Bot, Dispatcher
 from config import BOT_TOKEN
 from app.handlers.start import router
+
+from aiohttp import web
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 dp.include_router(router)
 
+# Фейковый веб-сервер
+async def fake_web_server():
+    async def handle(request):
+        return web.Response(text="OK")
+    app = web.Application()
+    app.router.add_get('/', handle)
+    port = int(os.environ.get("PORT", 8080))
+    runner = web.AppRunner(app)
+    await runner.setup()
+    site = web.TCPSite(runner, "0.0.0.0", port)
+    await site.start()
+
 async def main():
+    asyncio.create_task(fake_web_server())  # Запускаем фейковый сервер
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
