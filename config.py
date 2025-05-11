8f74325d9dff78bb5fa90c02bd25b1c703e911fc
Gitbottelegramserver <faton1234567891@gmail.com>
2025-05-11 17:43:39 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
-from dotenv import load_dotenv
 import os
-SELF_URL = os.getenv("SELF_URL", "https://algo-hub-bot.onrender.com")
+from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv()  # сначала загружаем .env
+
 BOT_TOKEN = os.getenv("BOT_TOKEN")
+SELF_URL = os.getenv("SELF_URL", "https://algo-hub-bot.onrender.com")
